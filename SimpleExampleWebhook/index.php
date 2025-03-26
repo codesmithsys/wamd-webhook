45583e072d299a2ad3afb7b5d6cb84fd6fef29aa
Codesmith Systems <codesmithsystems@gmail.com>
2025-03-26 23:46:18 +0300

    Add files via upload

---
+++ SimpleExampleWebhook/index.php
@@ -0,0 +1,97 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('error_log', 'error.log');
+
+require_once 'ResponWebhookFormatter.php';
+// this is simple php webhook for wmdb, not recommended using thi procedural pattern if you have a lot of keywords!
+header('content-type: application/json; charset=utf-8');
+$data = json_decode(file_get_contents('php://input'), true);
+if (!$data)  die('this url is for webhook.');
+
+file_put_contents('whatsapp.txt', '[' . date('Y-m-d H:i:s') . "]\n" . json_encode($data) . "\n\n", FILE_APPEND);
+$message = strtolower($data['message']); // this is incoming message from whatsapp
+$from = strtolower($data['from']); // this is the sender's whatsapp number
+$bufferimage = isset($data['bufferImage']) ? $data['bufferImage'] : null; // this is the image buffer if the message is image
+
+$respon = false; // 
+$responFormatter = new ResponWebhookFormater();
+// EXAMPLE RESPONS
+// to bold the message just add ->bold('text') before function respon;
+// to italic the message just add ->italic('text') before function respon;
+// to add new line the message just add ->line('text') before function respon;
+
+
+// -------------------------------------------
+
+// respon text
+if ($message == 'hello') {
+    $respon = $responFormatter->line('Hi too {name}')->line('Anything I can help?')->responAsText();
+}
+// respon text with quoted
+if ($message == 'hi') {
+    $respon = $responFormatter->quoted()->line('Hi too')->line('Anything I can help?')->responAsText();
+}
+// respon media (support imgage,audio,document and video)
+if ($message == 'media') {
+    $respon = $responFormatter->line('caption for you {name}')->responAsMedia('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/512px-WhatsApp.svg.png');
+}
+
+if($message == 'media2') {
+    $respon = $responFormatter->line('This is a document for you {name}')
+        ->responAsMedia('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', 'document', 'dummy.pdf');
+}
+// respon button
+if ($message == 'button') {
+    $respon = $responFormatter->line('ini isi pesannya')->bold("bold")
+        ->addButton("First button")
+        ->addButton("Second button")
+        ->addButton("Third button")
+        ->footer('this is the footer')
+        ->responAsButton();
+}
+// respon button with image
+if ($message == 'button image') {
+    $respon = $responFormatter->line('this is the message content')->bold("bold")
+        ->addButton("First button")
+        ->footer('this is the footer')
+        ->image('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/512px-WhatsApp.svg.png')
+        ->responAsButton();
+}
+
+// respon template button
+// you can also send image .. just add ->image('url_image') before ->responAsTemplateButton()
+if ($message == 'template button') {
+    $respon = $responFormatter->line('This is the content of the message.')->bold("bold")
+        ->addTemplateButton("First button (visit url when clicked)"https://google.com")
+        ->addTemplateButton("Second button (phone call when clicked)", "254722XXXXXX", "call")
+        ->footer('this is the footer')
+        ->responAsTemplateButton();
+}
+
+// respon list message
+if ($message == 'list') {
+    $respon = $responFormatter->line('this is the message content')->bold("bold")
+        ->addSection(
+            "First menu",
+            [
+                ['title' => 'List Item 1', 'rowId' => 'id2', 'description' => ''],
+                ['title' => 'List Item 2', 'rowId' => 'id3', 'description' => ''],
+            ]
+        )->addSection(
+            "Second menu",
+            [
+                ['title' => 'List Item 1', 'rowId' => 'id2', 'description' => ''],
+                ['title' => 'List Item 2', 'rowId' => 'id3', 'description' => ''],
+            ]
+        )->footer('this is the footer')
+        ->responAsList();
+}
+
+
+
+// save respon to file
+if ($respon) {
+    file_put_contents('respon.txt', '[' . date('Y-m-d H:i:s') . "]\n" . $respon . "\n\n", FILE_APPEND);
+}
+echo $respon;
---
+++ WebhookExample/ExampleManualMessage.php
@@ -0,0 +1,99 @@
+<?php
+
+// all method is for example only, you can modify it as you want.
+// if you want simple without manual format,you can use class ResponWebhookFormater.php
+class FormatMessage
+{
+    public static function text($text, $quoted = false)
+    {
+        return json_encode(['text' => $text, 'quoted' => $quoted]);
+    }
+
+    public static function exampleMedia($quoted = false)
+    {
+
+        return json_encode([
+            'url' => 'https://png.pngtree.com/element_our/md/20180626/md_5b321c99945a2.jpg',
+            'type' => 'image', // or 'video', 'document', 'audio'
+            'caption' => 'this is media for you {name}',
+            'filename' => 'image.jpg', // optional, if you want to change filename, default is 'media.ext'
+            'quoted' => $quoted,
+        ]);
+    }
+
+    public static function exampleButton($quoted = false)
+    {
+        // button
+        $buttons = [
+            [
+                'buttonId' => 'id1',
+                'buttonText' => ['displayText' => 'Button 1'],
+                'type' => 1,
+            ],
+            [
+                'buttonId' => 'id2',
+                'buttonText' => ['displayText' => 'Button 2'],
+                'type' => 1,
+            ],
+            [
+                'buttonId' => 'id3',
+                'buttonText' => ['displayText' => 'Button 3'],
+                'type' => 1,
+            ],
+        ];
+        $message = [
+            'text' => 'text',
+            'footer' => 'footer',
+            'headerType' => 1,
+            'viewOnce' => true,
+            'buttons' => $buttons,
+            'quoted' => $quoted,
+        ];
+
+        // if wnat to add image you can add like this to $message
+        // $message['image'] = ['url' => 'url_image']; and change text to caption
+
+        return json_encode($message);
+    }
+
+
+
+    public static function exampleList($quoted = false, $url = null)
+    {
+
+        $section = [
+            'list' => [
+                [
+                    'rows' => [
+                        [
+                            'title' => 'asdf',
+                            'description' => '--',
+                        ],
+                        [
+                            'title' => 'asdf',
+                            'description' => '--',
+                        ],
+                    ],
+                    'title' => 'asdf',
+                ],
+            ],
+            'buttonText' => 'asdf',
+        ];
+
+        $listMessage = [
+            'text' => 'asdfasf',
+            'footer' => 'asdf',
+            'sections' => [$section],
+            'buttonText' => 'asdf',
+        ];
+
+        if ($url) {
+            $listMessage['image'] = $url;
+            $listMessage['caption'] = 'caption';
+        }
+
+
+
+        return json_encode($listMessage);
+    }
+}
